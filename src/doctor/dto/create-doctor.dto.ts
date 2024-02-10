1873abdccf084e3b42716ed4b659e1c7155a76db
Hany Medhat <hany.medhat24@gmail.com>
2024-02-10 20:05:37 +0200

    create doctor class dto

--- src/doctor/dto/create-doctor.dto.ts
+++ src/doctor/dto/create-doctor.dto.ts
@@ -1,1 +1,31 @@
-export class CreateDoctorDto {}
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { Role } from 'src/auth/entities/enum/user.enum';
+
+export class CreateDoctorDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+  @IsNotEmpty()
+  @MinLength(8)
+  @IsString()
+  password: string;
+  @IsEnum({ Role, default: Role.DR })
+  role: Role;
+  @IsPositive()
+  @IsNumber({ maxDecimalPlaces: 0 })
+  department: number;
+  @IsString({ each: true })
+  college: string;
+  courses: number[];
+}
