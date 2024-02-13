411c2b5bace7fecd2ee894e2af0cf3ad8d8aafcc
Hany Medhat <hany.medhat24@gmail.com>
2024-02-13 19:30:40 +0200

    write create head of department

--- src/head-of-department/dto/create-head-of-department.dto.ts
+++ src/head-of-department/dto/create-head-of-department.dto.ts
@@ -1,1 +1,33 @@
-export class CreateHeadOfDepartmentDto {}
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { Role } from 'src/auth/entities/enum/user.enum';
+
+export class CreateHeadOfDepartmentDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+  @IsNotEmpty()
+  @MinLength(8)
+  @IsString()
+  password: string;
+  @IsEnum({ Role, default: Role.HOfDE })
+  role: Role;
+  @IsPositive()
+  @IsNumber({ maxDecimalPlaces: 0 })
+  department: number;
+  @IsString({ each: true })
+  college: string;
+  //   @IsPositive()
+  @IsNumber({}, { each: true })
+  teachingCourses: number[];
+}
