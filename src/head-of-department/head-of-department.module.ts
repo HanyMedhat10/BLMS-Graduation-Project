f0df0c6472d08be848dcf7fe96310dab42392d16
Hany Medhat <hany.medhat24@gmail.com>
2024-02-13 19:44:59 +0200

    create findAll , FindOne and delete

--- src/head-of-department/head-of-department.module.ts
+++ src/head-of-department/head-of-department.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HeadOfDepartmentService } from './head-of-department.service';
 import { HeadOfDepartmentController } from './head-of-department.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/auth/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [HeadOfDepartmentController],
   providers: [HeadOfDepartmentService],
 })
--- src/head-of-department/head-of-department.service.ts
+++ src/head-of-department/head-of-department.service.ts
@@ -1,26 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateHeadOfDepartmentDto } from './dto/create-head-of-department.dto';
 import { UpdateHeadOfDepartmentDto } from './dto/update-head-of-department.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/entities/user.entity';
+import { Repository } from 'typeorm/repository/Repository';
+import { Role } from 'src/auth/entities/enum/user.enum';
 
 @Injectable()
 export class HeadOfDepartmentService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
   create(createHeadOfDepartmentDto: CreateHeadOfDepartmentDto) {
     return 'This action adds a new headOfDepartment';
   }
 
-  findAll() {
-    return `This action returns all headOfDepartment`;
+  async findAll() {
+    return await this.userRepository.find({
+      where: { role: Role.HOfDE },
+      relations: {
+        department: true,
+        college: true,
+        teachingCourses: true,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} headOfDepartment`;
+  async findOne(id: number) {
+    const hod = await this.userRepository.findOne({
+      where: { id: id, role: Role.HOfDE },
+      relations: {
+        department: true,
+        college: true,
+        teachingCourses: true,
+      },
+    });
+    if (!hod) throw new NotFoundException('Head Of Department not found');
+    return hod;
   }
 
   update(id: number, updateHeadOfDepartmentDto: UpdateHeadOfDepartmentDto) {
     return `This action updates a #${id} headOfDepartment`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} headOfDepartment`;
+  async remove(id: number) {
+    const hod = await this.findOne(id);
+    return await this.userRepository.delete(hod.id);
   }
 }
