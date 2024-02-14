ccc9bf1ce69c1d8297f8f4df3036a84eb91d050b
Hany Medhat <hany.medhat24@gmail.com>
2024-02-14 22:59:53 +0200

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 import * as process from 'process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
+  const config = new DocumentBuilder()
+    .setTitle('BLMS example')
+    .setDescription('The BLMS API description')
+    .setVersion('1.0')
+    .addTag('BLMS')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
