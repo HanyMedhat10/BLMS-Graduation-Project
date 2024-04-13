754ab2495bc8bdaec1423cbdf95c9696d8b6623d
Hany Medhat <hany.medhat24@gmail.com>
2024-04-13 17:17:36 +0200

    create chat dto

--- src/chat/dto/create-chat.dto.ts
+++ src/chat/dto/create-chat.dto.ts
@@ -1,1 +1,26 @@
-export class CreateChatDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class CreateChatDto {
+  @ApiProperty()
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  chatId: number;
+  @ApiProperty()
+  @IsInt()
+  @Min(1)
+  receiverId: number;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+}
