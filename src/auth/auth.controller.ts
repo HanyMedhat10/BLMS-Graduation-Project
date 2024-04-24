b4699fc110d6f172ecebe3d6b22703ecda868946
Hany Medhat <hany.medhat24@gmail.com>
2024-04-24 08:28:59 +0200

    Increase the maximum file size allowed by the MaxFileSizeValidator from 2000 bytes to 2000000 bytes to accommodate larger files.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -147,7 +147,7 @@ export class AuthController {
     @UploadedFile(
       new ParseFilePipe({
         validators: [
-          new MaxFileSizeValidator({ maxSize: 2000 }), // 2000*bytes
+          new MaxFileSizeValidator({ maxSize: 2000000 }), // 2000*bytes
           new FileTypeValidator({
             fileType: 'image/jpeg|image/png', // jpeg and png formats,
           }),
